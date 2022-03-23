b942dc1290c8f5af4e03a9c6dec8872572f5d266
Marco <marcooliveri94@gmail.com>
2022-03-23 10:31:30 +0100

    .

--- functions.php
+++ functions.php
@@ -13,8 +13,8 @@
 * - SHORTCODE WITH ATTRIBUTES
 * - SHORTCODE + ACF
 * - WP QUERY
-* - WP QUERY + ACF
-* - WP QUERY + ACF (Import only products chosen in backend artisan or owner page)
+* - ACF QUERY
+* - ACF QUERY (Import only products chosen in backend artisan or owner page)
 * - REGISTER A POST TYPE
 * - CHANGE WORDPRESS LOGIN PANEL LOGO
 * - SEND EMAIL WHEN POST IS PUBLISHED
@@ -180,49 +180,23 @@ if ($query->have_posts()) {
 wp_reset_postdata();
 
 
-/* ======== WP QUERY + ACF ======== */
-/* Custom products query from a chosen tag */
+/* ======== ACF REPEATER ======== */
+/* Custom products chosen in backend panel */
+
 
 function custom_products_block() {
-	$post_id = get_field("custom_field_name"); // I used ACF because I want to filter products of a chosen meta tag.
-	$post_slug = $post_id->slug;
+	$posts = get_field("prodotti"); //Custom field has to be Post Object
+	if($posts) {
+	
+foreach($posts as $post) {
+	$post_title = get_the_title($post);
+	$post_thumb = get_the_post_thumbnail($post);
 
-	// I'm going to choose arguments for my query
-	$args = array(
-    'post_type'              => array('product'), // use any for any kind of post type, custom post type slug for custom post type
-    'post_status'            => array('publish'), // Also support: pending, draft, auto-draft, future, private, inherit, trash, any
-    'posts_per_page'         => '4', // use -1 for all post
-    'order'                  => 'DESC', // Also support: ASC
-    'orderby'                => 'date', // Also support: none, rand, id, title, slug, modified, parent, menu_order, comment_count
-    'tax_query'              => array(
-        'relation' => 'AND', // Use AND for taking result on both condition true
-        array(
-            'taxonomy'         => 'product_tag', // taxonomy slug
-            'terms'            => $post_slug, // term ids
-            'field'            => 'slug', // Also support: slug, name, term_taxonomy_id
-        ),
-    ),
-);
- 
-// Start the query with selected arguments above.
-$the_query = new WP_Query($args);
- 
-// If posts are in, than I'll loop until the end.
-if ( $the_query->have_posts() ) {
-	echo '<ul class="products elementor-grid columns-4">'; //Some elementor classes;
-    while($the_query->have_posts() ) {
-		$the_query->the_post();
-		/* I will use default product template from woocommerce or your theme */
-		wc_get_template_part( 'content', 'product' );
-    }
-	echo '</ul>';
-} else {
-  echo 'Nessun prodotto trovato';
+$products = '<h1>' . $post_title . '</h1><img src="' . $post_thumb .  '" />';
 }
-
-wp_reset_postdata();
 }
-add_shortcode('custom_metaProducts', 'custom_products_block');
+}
+add_shortcode('products_tab', 'custom_products_block');
 
 
 /* WP QUERY + ACF (Import only products chosen in backend artisan or owner page) */
